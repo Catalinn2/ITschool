e0da9ea1011333e270422dc97d49c9a583367cc2
Catalinn2 <catalyn102@gmail.com>
2023-09-29 21:29:26 +0300

    Session9Practice

---
+++ src/session7/challanges/Challange1.java
@@ -0,0 +1,7 @@
+package session7.challanges;
+
+public class Challange1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/session9/practice/book_management_system/Authors.java
@@ -0,0 +1,14 @@
+package session9.practice.book_management_system;
+
+public class Authors {
+
+    private String name;
+
+    public Authors(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
